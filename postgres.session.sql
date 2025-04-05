5823d44ae59e46a341499e21876d5397280613e8
mtszczepaniak <mateusz.szczepaniak7@gmail.com>
2025-04-05 20:39:01 +0200

    commit

---
+++ postgres.session.sql
@@ -0,0 +1,60 @@
+CREATE TABLE styczen (
+    WOJEWODZTWO INTEGER,
+    MARKA TEXT,
+    LICZBA INTEGER
+);
+
+SELECT*
+FROM styczen
+
+
+CREATE TABLE luty (
+    WOJEWODZTWO INTEGER,
+    MARKA TEXT,
+    LICZBA INTEGER
+);
+
+ALTER TABLE luty
+ALTER COLUMN WOJEWODZTWO TYPE TEXT;
+
+COPY styczen 
+FROM 'C:\Users\MateuszSz\Desktop\doc\PL_Car_registration\MARKI_OSOBOWE_2025_01.csv' DELIMITER ',' CSV HEADER;
+
+COPY luty
+FROM 'C:\Users\MateuszSz\Desktop\doc\PL_Car_registration\MARKI_OSOBOWE_2025_02.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE styczen
+ADD COLUMN MIESIAC TEXT;
+
+ALTER TABLE luty
+ADD COLUMN MIESIAC TEXT;
+
+UPDATE styczen
+SET miesiac = '1';
+
+UPDATE luty
+SET miesiac = '2';
+
+SELECT*
+FROM luty;
+
+SELECT * FROM styczen
+UNION ALL
+SELECT * FROM luty;
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public';
+
+CREATE TABLE ROK25 AS
+SELECT * FROM styczen
+UNION ALL
+SELECT * FROM luty;
+
+SELECT*
+FROM ROK25;
+
+CREATE TABLE MARKI AS
+SELECT DISTINCT MARKA 
+FROM ROK25
+ORDER BY marka;
